0989fc0a24535054d9a77728bf54fb8f9e1f5aae
simond <simon.dobner@psma.com.au>
2018-11-22 21:05:53 +1100

    Pythonise varible names

--- statement.py
+++ statement.py
@@ -3,8 +3,8 @@ from math import floor
 
 
 def statement(invoice, plays):
-    totalAmount = 0
-    volumeCredits = 0
+    total_amount = 0
+    volume_credits = 0
     invoice = json.loads(invoice)
     plays = json.loads(plays)
     result = f"Statement for {invoice[0]['customer']}\n"
@@ -20,42 +20,42 @@ def statement(invoice, plays):
 
     for perf in invoice[0]['performances']:
         play = plays[perf['playID']]
-        thisAmount = 0
+        this_amount = 0
 
         if play['type'] == 'tragedy':
             if play['type'] == "tragedy":
-                thisAmount = 40000
+                this_amount = 40000
 
             if perf['audience'] > 30:
-                thisAmount += 1000 * (perf['audience'] - 30)
+                this_amount += 1000 * (perf['audience'] - 30)
 
         elif play['type'] == 'comedy':
-            thisAmount = 30000
+            this_amount = 30000
             if perf['audience'] > 20:
-                thisAmount += 10000 + 500 * (perf['audience'] - 20)
-            thisAmount += 300 * perf['audience']
+                this_amount += 10000 + 500 * (perf['audience'] - 20)
+            this_amount += 300 * perf['audience']
         else:
             raise Exception(f'unknown type: {play["type"]}')
 
         # add volume credits
-        volumeCredits += max(perf['audience'] - 30, 0)
+        volume_credits += max(perf['audience'] - 30, 0)
         # add extra credit for every ten comedy attendees
 
         if "comedy" == play['type']:
-            volumeCredits += floor(perf['audience'] / 5)
+            volume_credits += floor(perf['audience'] / 5)
 
         # print line for this order
-        result += f"{play['name']}: {thisAmount/100} {perf['audience']} (seats)\n"
-        totalAmount += thisAmount
+        result += f"{play['name']}: {this_amount/100} {perf['audience']} (seats)\n"
+        total_amount += this_amount
 
-    result += f'Amount owed is {totalAmount/100}\n'
-    result += f'You earned {volumeCredits} credits\n'
+    result += f'Amount owed is {total_amount/100}\n'
+    result += f'You earned {volume_credits} credits\n'
 
     return result
 
-        # Statement for BigCo
-        #   Hamlet: $650.00 (55 seats)
-        #   As You Like It: $580.00 (35 seats)
-        #   Othello: $500.00 (40 seats)
-        # Amount owed is $1,730.00
-        # You earned 47 credits
+    # Statement for BigCo
+    #   Hamlet: $650.00 (55 seats)
+    #   As You Like It: $580.00 (35 seats)
+    #   Othello: $500.00 (40 seats)
+    # Amount owed is $1,730.00
+    # You earned 47 credits
