a50836b899a4c1af5f9d59428876f7056f5dd9e3
simond <simon.dobner@psma.com.au>
2018-11-22 18:52:26 +1100

    Converted to python

--- statement.py
+++ statement.py
@@ -1,4 +1,5 @@
 import json
+from math import floor
 
 
 def statement(invoice, plays):
@@ -24,7 +25,6 @@ def statement(invoice, plays):
                 thisAmount += 1000 * (perf.audience - 30)
 
         elif play['type'] == 'comedy':
-            None
             thisAmount = 30000
             if perf['audience'] > 20:
                 thisAmount += 10000 + 500 * (perf.audience - 20);
@@ -32,17 +32,17 @@ def statement(invoice, plays):
         else:
             raise Exception(f'unknown type: {play["type"]}')
 
-    #
-    #   // add volume credits
-    #   volumeCredits += Math.max(perf.audience - 30, 0);
-    #   // add extra credit for every ten comedy attendees
-    #   if ("comedy" === play.type) volumeCredits += Math.floor(perf.audience / 5);
-    #
-    #   //print line for this order
-    #   result += `  ${play.name}: ${format(thisAmount/100)} (${perf.audience} seats)\n`;
-    #   totalAmount += thisAmount;
-    # }
-    # result += `Amount owed is ${format(totalAmount/100)}\n`;
-    # result += `You earned ${volumeCredits} credits\n`;
+        # add volume credits
+        volumeCredits += max(perf.audience - 30, 0)
+        # add extra credit for every ten comedy attendees
+        if "comedy" == play['type']:
+            volumeCredits += floor(perf['audience'] / 5)
+
+        # print line for this order
+        result += f'{play.name}: {thisAmount/100} {perf.audience} seats)\n'
+        totalAmount += thisAmount
+
+    result += f'Amount owed is {totalAmount/100}\n'
+    result += f'You earned {volumeCredits} credits\n'
 
     return result
